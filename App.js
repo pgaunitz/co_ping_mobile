6a4097cd4dfb53b44c166ce6d48a48a0a0f0098c
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-13 12:08:19 +0200

    deleted files determined to be unnecessary at this point, added sample text and changed background color to see if image was displayed
    Co-authored-by: johan <johan@waytogo.se>
    Co-authored-by: karro <karolina.frostare@gmail.com>

--- App.js
+++ App.js
@@ -1,67 +1,22 @@
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { View, Text, Image, StyleSheet} from "react-native";
+import co_ping_Logo2 from "./assets/images/co_ping_Logo2.png";
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
-
-const Stack = createStackNavigator();
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
-}
+const App = () => {
+  return (
+    <View style={styles.mainpage}>
+      <Image source={co_ping_Logo2} />
+      <Text>New Trip</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
+  mainpage: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#134E5E',
   },
 });
+
+export default App;
+
