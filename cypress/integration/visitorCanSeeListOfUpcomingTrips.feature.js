0026dc2928b9f22932378c1e40c03e10e0e61430
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-14 16:41:07 +0200

    created tests and tixture file for user can see list of upcoming trips
    Co-authored-by: karro <karolina.frostare@gmail.com>
    Co-authored-by: johan <johan@waytogo.se>

---
+++ cypress/integration/visitorCanSeeListOfUpcomingTrips.feature.js
@@ -0,0 +1,27 @@
+describe("Visitor can see list of all upcoming active trips", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "**/pings",
+      response: "fixture:trip_list.json",
+    });
+    cy.visit("/");
+  });
+
+  it("sucessfully.", () => {
+    cy.get("#trip-list-button").click();
+    cy.get("#trip-list").should("contain", "Coop");
+    cy.get("#trip-list").should("contain", "2020-04-14 14:00");
+    cy.get("#trip-list").should("contain", "Systembolaget");
+    cy.get("#trip-list").should("contain", "2020-04-14 10:00");
+  });
+
+  it("unsuccessfully", () => {
+    cy.get("#trip-list-button").click();
+    cy.get("#trip-list").should(
+      "contain",
+      "Sorry, no active trips at this time."
+    );
+  });
+});
