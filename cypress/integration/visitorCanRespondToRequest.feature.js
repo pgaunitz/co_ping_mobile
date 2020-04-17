e9c3b5740ae93b520511378cf68fb93ef3441c43
bounce <johan@waytogo.se>
2020-04-17 12:19:38 +0200

    added and configured fixture files, continued writing tests for user can accept or reject requests
    Co-authored-by: kayla <kaylasandoval92@gmail.com>
    Co-authored-by: karro <karolina.frostare@gmail.com>

--- cypress/integration/visitorCanRespondToRequest.feature.js
+++ cypress/integration/visitorCanRespondToRequest.feature.js
@@ -4,17 +4,17 @@ describe("Visitor can", () => {
     cy.route({
       method: "PUT",
       url: "**/**",
-      response: "fixture:request_response.json"
+      response: "fixture:request_response.json",
     });
     cy.route({
       method: "GET",
-      url: "**/pongs/**", 
-      response: "fixture:trip_requests_list.json"
+      url: "**/pongs/**",
+      response: "fixture:trip_details",
     });
     cy.route({
       method: "POST",
       url: "**/pings",
-      response: "fixture:new_trip_response.json"
+      response: "fixture:new_trip_response.json",
     });
     cy.route({
       method: "POST",
@@ -25,36 +25,45 @@ describe("Visitor can", () => {
       method: "GET",
       url: "**/pings",
       response: "fixture:trip_list.json",
+    });
+    cy.route({
+      method: "POST",
+      url: "**/pongs/**",
+      response: "fixture:trip_requests_list.json",
     })
-    cy.visit("/")
-    cy.window().then(window => {
+
+    cy.visit("/");
+    cy.window().then((window) => {
       window.store.dispatch({
         type: "AUTHENTICATE",
-        payload: { authenticated: true, userId: 1 }
+        payload: { authenticated: true, userId: 1 },
       });
     });
-    cy.get("#trip-list-button").click()
-    cy.get("#new-trip-button").click();
-    cy.get("#time").type("2020-04-13 16:30");
-    cy.get("#store").type("ICA");
-    cy.get("#create-trip-button")
-      .contains("Create")
-      .click();
-    cy.get("#close-trip-form").click();
-    cy.get("#trip-button").click();
-    cy.get(".request-form").should("contain", "Trip Request");
-    cy.get("#item-one").type("A");
-    cy.get("#item-two").type("B");
-    cy.get("#item-three").type("C");
-    cy.get("#submit-request").click();
-    cy.get("[data-testID=header-back]").last().click({force: true}); 
-    cy.get("#new-trip-button").should('have.text', "New Ping");
-    cy.get("[data-testID=header-back]").last().click({force: true});
-    cy.get("#request-list-button").should('have.text', "My shopPing Board")
+    // cy.get("#trip-list-button").click()
+    // cy.get("#new-trip-button").click();
+    // cy.get("#time").type("2020-04-13 16:30");
+    // cy.get("#store").type("ICA");
+    // cy.get("#create-trip-button")
+    //   .contains("Create")
+    //   .click();
+    // cy.get("#close-trip-form").click();
+    // cy.get("#trip-button").click();
+    // cy.get(".request-form").should("contain", "Trip Request");
+    // cy.get("#item-one").type("A");
+    // cy.get("#item-two").type("B");
+    // cy.get("#item-three").type("C");
+    // cy.get("#submit-request").click();
+    // cy.get("[data-testID=header-back]").last().click({force: true});
+    // cy.get("#new-trip-button").should('have.text', "New Ping");
+    // cy.get("[data-testID=header-back]").last().click({force: true});
+    // cy.get("#request-list-button").should('have.text', "My shopPing Board")
   });
 
   it("accept a request", () => {
-
+    cy.get("#request-list-button").click();
+    cy.get("body").should("contain", "ICA");
+    cy.get("body").should("contain", "2020-04-13-16:30");
+    cy.get("#1").should("contain", "Awesome Girl")
   });
 
   xit("reject a request", () => {});
