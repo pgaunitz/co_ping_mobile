03d332f441de2180381d82ce7d2b67725b78c72b
karolina <karolina.frostare@gmail.com>
2020-04-19 14:20:02 +0200

    creates test for user to complete trip, creates and populates fixture files
    Co-authored-by: kayla <kaylasandoval92@gmail.com>
    Co-authored-by: johan <johan@waytogo.se>

--- cypress/integration/visitorCanCloseThierTrip.feature .js
+++ cypress/integration/visitorCanCloseThierTrip.feature .js
@@ -4,33 +4,40 @@ describe("Visitor can", () => {
     cy.route({
       method: "GET",
       url: "**/pings/**",
-      response: "fixture:trip_details.json",
+      response: "fixture:trip_details.json"
     });
     cy.route({
       method: "POST",
       url: "**/auth/**",
-      response: "fixture:user_login.json",
+      response: "fixture:user_login.json"
     });
     cy.route({
       method: "GET",
       url: "**/auth/**",
-      response: "fixture:user_login.json",
+      response: "fixture:user_login.json"
     });
     cy.route({
       method: "PUT",
       url: "**/pings/**",
-      response: "fixture:close_trip_response.json",
+      response: "fixture:close_trip_response.json"
     });
 
     cy.visit("/");
-    cy.get("#login-button").contains("Login").click();
+    cy.get("#login-button")
+      .contains("Login")
+      .click();
     cy.get("#email").type("user@mail.com");
     cy.get("#password").type("password");
-    cy.get("#submit-login").contains("Submit").click();
+    cy.get("#submit-login")
+      .contains("Submit")
+      .click();
     cy.get("#request-list-button").click();
-  })
+  });
   it("can close a trip to new request", () => {
-    cy.get("#close-trip-button").click()
-    cy.get("#close-trip-message").should("contain", "You are ready to go shopping, don't forget the receipts!")
-  })
-})
+    cy.get("#close-trip-button").click();
+    cy.get("#close-trip-message").should(
+      "contain",
+      "You are ready to go shopping, don't forget the receipts!"
+    );
+  });
+});
