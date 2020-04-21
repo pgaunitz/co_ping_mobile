c99f41d3c8b73793abb63f2a0dbac882771f6082
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-21 12:16:42 +0200

    added text input field and and submit button, minor styling
    Co-authored-by: johan <johan@waytogo.se>

--- screens/SignUp.jsx
+++ screens/SignUp.jsx
@@ -1,8 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import { LinearGradient } from "expo-linear-gradient"
-import { TouchableHighlight, View, StyleSheet, Text, Image } from "react-native"
+import { TouchableHighlight, View, StyleSheet, Text, TextInput } from "react-native"
+import { sendCommunityCode } from "../modules/signUp"
+import { useSelector, useDispatch } from "react-redux";
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const [code, onChangeCode] = useState();
+
   return (
     <View style={styles.mainpage}>
       <LinearGradient
@@ -10,7 +15,27 @@ const SignUp = () => {
         style={{ flex: 1 }}
         start={{ x: 0, y: 1 }}
         end={{ x: 1, y: 0 }}
-      ></LinearGradient>
+      >
+        <Text style={styles.title}>Sign up</Text>
+
+        <TextInput
+          placeholder="Enter community code here"
+          id="secret-code"
+          style={styles.dataInput}
+          value={code}
+          onChangeText={code => onChangeCode(code)}
+        />
+        <TouchableHighlight
+          style={styles.request}
+          onPress={() => {
+            sendCommunityCode(code, dispatch);
+          }}
+        >
+          <Text id="code-submit-button" style={styles.requestButtonText}>
+            Submit code
+            </Text>
+        </TouchableHighlight>
+      </LinearGradient>
     </View>
   )
 }
@@ -18,6 +43,34 @@ const SignUp = () => {
 const styles = StyleSheet.create({
   mainpage: {
     flex: 1,
+  },
+  title: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+    marginBottom: 20,
+    marginTop: 50
+  },
+  dataInput: {
+    border: 1,
+    backgroundColor: "white",
+    color: "black",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 200,
+    padding: 10,
+    borderRadius: 3
+  },
+  request: {
+    borderRadius: 10,
+    backgroundColor: "#71B280",
+    marginTop: 15,
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: 10,
+    width: "40%",
+    justifyContent: "center",
+    alignItems: "center"
   },
 })
 
